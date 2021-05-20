4bff4e14199fb5e848e2b6b2e6912f3e1f2decb2
User <user.push@hackerrank.workspace>
2021-05-20 02:42:44 +0000

    [Admin Commit] save changes
    Commit by HackerRank

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,14 +2,14 @@ var recipes = require('../recipes.json');
 var router = require('express').Router();
 
 const Recipes = {
-  get: async function() {
-    const res = await Object.keys(recipesList).map(key => recipesList[key]);
+  get: function() {
+    const res = Object.keys(recipesList).map(key => recipesList[key]);
     return res;
   }
 }
 
-router.get('/', async (req, res) => {
-  const recipes = await Recipes.get();
+router.get('/', (req, res) => {
+  const recipes = Recipes.get();
   res.status(200).json(recipes);
 });
 
