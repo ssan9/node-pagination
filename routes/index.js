64466c50eb29727f4d9cdccba95fbecc9656ba44
User <user.push@hackerrank.workspace>
2021-05-20 02:07:02 +0000

    [Admin Commit] save changes
    Commit by HackerRank

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const { router: recipeRouter} = require('./recipes');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('<p>HTML Data</p>');
 });
-
-app.use('/recipes', recipeRouter);
 
 module.exports = router;
