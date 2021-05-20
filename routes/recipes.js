d4bde5c0e893ab968983f3c5238fa843800e3c37
ssan9 <perrsy@gmail.com>
2021-05-20 14:05:41 -0400

    added pagination

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,17 +2,24 @@ var recipes = require('../recipes.json');
 var router = require('express').Router();
 
 const Recipes = {
-  get: function() {
-    const res = Object.keys(recipesList).map(key => recipesList[key]);
-    return res;
+  get: function(query) {
+    let { page } = query;
+    let { limit } = query;
+    if (!page) {
+      page = 1
+    }
+    if (!limit) {
+      limit = 3
+    }
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    return recipes.slice(startIndex, endIndex);
   }
-}
+};
 
-router.get('/', (req, res) => {
-  const recipes = Recipes.get();
-  res.status(200).json(recipes);
+router.get('/:page?/:limit?', (req, res) => {
+  res.status(200).json(Recipes.get(req.params));
 });
-
 
 module.exports = router;
 
