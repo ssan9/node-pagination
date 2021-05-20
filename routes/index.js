af8a5f46faab8e0f79d7a7c66b489824bfc10144
User <user.push@hackerrank.workspace>
2021-05-20 02:03:23 +0000

    [Admin Commit] save changes
    Commit by HackerRank

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const router = require('./recipes');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('<p>HTML Data</p>');
 });
+
+app.use('/recipes', router);
 
 module.exports = router;
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,15 @@
 var recipes = require('../recipes.json');
 var router = require('express').Router();
 
+const Recipes = {
+  get: function() {
+    return Object.keys(this.items).map(key => this.item[key]);
+  }
+}
+
+router.get('/', (req, res) => {
+  res.json(Recipes.get());
+});
+
 module.exports = router;
 
