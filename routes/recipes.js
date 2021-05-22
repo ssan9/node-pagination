d6d80aebf883cd76f1987c5e44d0c98298b4185d
viveksinghscheduleonce <viveksingh101994@gmail.com>
2021-05-22 21:40:35 +0530

    refactor: :recycle: refactored the code for controller

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,23 +2,16 @@ var recipes = require('../recipes.json');
 var router = require('express').Router();
 
 const Recipes = {
-  get: function(query) {
-    let { page } = query;
-    let { limit } = query;
-    if (!page) {
-      page = 1
-    }
-    if (!limit) {
-      limit = 3
-    }
+  get: function(page,limit) {
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
     return recipes.slice(startIndex, endIndex);
   }
 };
 
-router.get('/:page?/:limit?', (req, res) => {
-  res.status(200).json(Recipes.get(req.params));
+router.get('/', (req, res) => {
+  let {query:{page=1,limit=3}} = req;
+  res.status(200).json(Recipes.get(page,limit));
 });
 
 module.exports = router;
