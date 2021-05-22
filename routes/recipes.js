a4618327226f385bda0350f8d542582660b4229b
ssan9 <perrsy@gmail.com>
2021-05-22 16:58:04 -0400

    refactored

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,15 +2,7 @@ var recipes = require('../recipes.json');
 var router = require('express').Router();
 
 const Recipes = {
-  get: function(query) {
-    let { page } = query;
-    let { limit } = query;
-    if (!page) {
-      page = 1
-    }
-    if (!limit) {
-      limit = 3
-    }
+  get: function(page, limit) {
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
     return recipes.slice(startIndex, endIndex);
@@ -18,7 +10,8 @@ const Recipes = {
 };
 
 router.get('/', (req, res) => {
-  res.status(200).json(Recipes.get(req.query));
+  let {query:{page=1,limit=3}} = req;
+  res.status(200).json(Recipes.get(page,limit));
 });
 
 module.exports = router;
